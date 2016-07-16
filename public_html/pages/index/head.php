2cc86d73f0d0632b745b326da0591239dea98233
SaladRaider <peterkangveerman@gmail.com>
2016-07-15 21:08:56 -0700

    Added a valid google api client id to the login api

--- public_html/pages/index/head.php
+++ public_html/pages/index/head.php
@@ -5,7 +5,7 @@
  * Time: 11:23 AM
  */
 ?>
-<meta name="google-signin-client_id" content="YOUR_CLIENT_ID.apps.googleusercontent.com">
+<meta name="google-signin-client_id" content="394558371353-92gvgku6h44tcikbg29qkcvsu7pfeck7.apps.googleusercontent.com">
 <link rel="stylesheet" type="text/css" href="/pages/index/style/index.css">
 <script src="pages/index/js/Interactives.js"></script>
 <script src="https://apis.google.com/js/platform.js?onload=renderButton" async defer></script>
--- public_html/pages/index/js/GoogleInit.js
+++ public_html/pages/index/js/GoogleInit.js
@@ -1,9 +1,15 @@
 function onSuccess(googleUser) {
-	console.log('Logged in as: ' + googleUser.getBasicProfile().getName());
+	var profile = googleUser.getBasicProfile();
+	console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+	console.log('Name: ' + profile.getName());
+	console.log('Image URL: ' + profile.getImageUrl());
+	console.log('Email: ' + profile.getEmail());
 }
+
 function onFailure(error) {
 	console.log(error);
 }
+
 function renderButton() {
 	gapi.signin2.render('my-signin2', {
 		'scope': 'profile email',
@@ -14,11 +20,4 @@ function renderButton() {
 		'onsuccess': onSuccess,
 		'onfailure': onFailure
 	});
-}
-function onSignIn(googleUser) {
-	var profile = googleUser.getBasicProfile();
-	console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-	console.log('Name: ' + profile.getName());
-	console.log('Image URL: ' + profile.getImageUrl());
-	console.log('Email: ' + profile.getEmail());
 }
